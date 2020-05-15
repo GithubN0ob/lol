44d2ca6b31a194f50a8462bca8ea5feaf52f393a
GithubN0ob <simolanian@gmail.com>
2020-05-15 14:06:34 +0900

    Updated send button coordinates

--- idk.py
+++ idk.py
@@ -5,10 +5,10 @@ from string import ascii_lowercase
 print("running.")
 
 def tap(x, y):
-	os.system("adb shell input tap {x} {y}")
+	os.system(f"adb shell input tap {x} {y}")
 
 def type(text):
-	os.system('adb shell input text "{text}"')
+	os.system(f'adb shell input text "{text}"')
 
 def word_generator(x):
 	text = ""
@@ -20,9 +20,9 @@ def word_generator(x):
 # tap(250, 2100) #chat
 # tap(500, 500)
 
-for i in range(5):
+for i in range(100):
 	type(word_generator(14))
-	tap(1000, 1280)
+	tap(1050, 1420)
 
 """
 cd documents\desktop
